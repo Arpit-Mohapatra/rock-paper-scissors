db4ee87e02a57841f926c337d33b7e98c2043378
Arpit Mohapatra <mohapatra.arpit2003@cet.edu.in>
2022-11-08 01:42:34 +0530

    Console game version

--- JS/app.js
+++ JS/app.js
@@ -1,11 +1,43 @@
 // console.log("Hello World!");
 // window.prompt("Enter your choice");
+let computerSelection, playerSelection,playerScore=0,computerScore=0;
 function getComputerChoice(){
-    let randomNumber=Math.floor(Math.random()* 100)+1;
-    if(randomNumber<=33)
-    return "rock";
-    else if(randomNumber<=66)
-    return "paper";
+    computerSelection=Math.floor(Math.random()* 100)+1;
+    if(computerSelection<=33)
+    return ("rock") ;
+    else if(computerSelection<=66)
+    return ("paper") ;
     else
-    return "scissors";
+    return ("scissors") ;
 }
+// getPlayerChoice();
+// getComputerChoice();
+function playRound(){
+    while(playerScore<5&& computerScore<5){
+        computerSelection=getComputerChoice();
+        playerSelection=getPlayerChoice();
+        if((playerSelection==="rock"&&computerSelection==="scissors")|| (playerSelection==="paper"&&computerSelection==="rock")|| (playerSelection==="scissors"&&computerSelection==="paper")){
+            playerScore++;
+            console.log("Your score = "+playerScore);
+        }
+        else if((computerSelection==="rock"&&playerSelection==="scissors")|| (computerSelection==="paper"&&playerSelection==="rock")|| (computerSelection==="scissors"&&playerSelection==="paper")){
+            computerScore++;
+            console.log("Computer score = "+computerScore);
+
+        }
+    }
+}
+function displayWinner(){
+    if(playerScore==5)
+    console.log("You won");
+    else
+    console.log("Computer won");
+}
+function getPlayerChoice(){
+    playerSelection=prompt("Enter rock,paper or scissors");
+    playerSelection=playerSelection.trim();
+    return (playerSelection.toLowerCase()) ;
+}
+
+playRound();
+displayWinner();
